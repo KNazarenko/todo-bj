b363db0c99f11ccec74f3736783edf7e7cbad341
knazarenko <nko@ukr.net>
2019-03-01 23:32:51 +0200

    Updates

---
+++ precache-manifest.cd66815259bb132064ba56aa2136192f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6443353d38f1b03c2023",
+    "url": "/todo-bj/static/js/runtime~main.6443353d.js"
+  },
+  {
+    "revision": "b8c430163c68cb4a7dad",
+    "url": "/todo-bj/static/js/main.b8c43016.chunk.js"
+  },
+  {
+    "revision": "2c60ed3b654017f198f1",
+    "url": "/todo-bj/static/js/2.2c60ed3b.chunk.js"
+  },
+  {
+    "revision": "b8c430163c68cb4a7dad",
+    "url": "/todo-bj/static/css/main.79239086.chunk.css"
+  },
+  {
+    "revision": "2c60ed3b654017f198f1",
+    "url": "/todo-bj/static/css/2.ccebe112.chunk.css"
+  },
+  {
+    "revision": "60c97bba40d7fc4fccd67c0ca1fb5d47",
+    "url": "/todo-bj/index.html"
+  }
+];
